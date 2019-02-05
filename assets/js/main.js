71bc3e861ffb79f41dba08ba7845c9bca299c8bf
Richard Nii Lante Lawson <niilante@edenteq.com>
2019-02-05 17:38:36 +0000

    v0.0.1

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+
+// Transparent Sticky NavBar
+
+$(document).ready(function() {
+    $(".menu-icon").on("click", function() {
+          $("nav ul").toggleClass("showing");
+    });
+});
+
+// Scrolling Effect
+
+$(window).on("scroll", function() {
+    if($(window).scrollTop()) {
+          $('nav').addClass('black');
+    }
+
+    else {
+          $('nav').removeClass('black');
+    }
+})
+
+// End Transparent Sticky NavBar
